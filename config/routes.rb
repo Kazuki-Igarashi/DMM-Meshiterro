8c5e60f646aaf9c92b860a7d7f01bf8f202d57fd
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-03 07:39:08 +0000

    14章 ユーザーに関する機能(1) - 準備編

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  # get 'users/show'
+  # get 'users/edit'
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
   resources :post_images, only: [:new, :create, :index, :show]
+  resources :users, only: [:show, :edit]
   devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get 'homes/top'
   get 'homes/about' => 'homes#about', as: 'about'
   root to: "homes#top"
