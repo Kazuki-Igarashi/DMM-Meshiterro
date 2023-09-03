ed690972b1f27a6101bad6e4a1a4ee6a0be215be
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-03 06:43:58 +0000

    Meshiterro 10章 投稿機能の作成(3) - 投稿できるようにしよう

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   devise_for :users
   get 'homes/top'
   get 'homes/about' => 'homes#about', as: 'about'
